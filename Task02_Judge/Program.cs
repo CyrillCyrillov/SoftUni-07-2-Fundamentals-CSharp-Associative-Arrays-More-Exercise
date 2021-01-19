63d145023ad0432bb161f097dc2c413e4e5dca97
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:46:53 +0200

    Add files via upload

---
+++ Task02_Judge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task02_Judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
